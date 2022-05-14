f04fca54d7163a00b0443fa14a4465bdede542e2
Eugen <t0lrk0ln22rus@mail.ru>
2022-05-14 16:46:37 +0300

    one

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-let s;
+let number = +prompt(meaning);
+if (typeof number === 'number'){
+		if (i % 2 === 0 && i % 2 != isNaN(isNaN(i))){
+			console.log(`Число ${number} четное`);
+		} else{
+			console.log(`Число ${number} не четное`);
+		}
+} else{
+	console.log('Упс, кажется, вы ошиблись');
+}
+
